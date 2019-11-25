95ae9f1d16c3e4f0971f69092f3ea7c6e64d0995
Colin Zhang <l8v0b@ugrad.cs.ubc.ca>
2019-11-24 16:07:16 -0800

    minor addition to script

--- create_tables.sql
+++ create_tables.sql
@@ -100,8 +100,10 @@ create table RentReturn (
 commit;
 
 insert into branch(location,city) values ('Blundell and No. 3', 'Richmond');
+insert into branch(location,city) values ('Steveston', 'Richmond');
 insert into branch(location,city) values ('Waterfront', 'Vancouver');
 insert into branch(location,city) values ('Lougheed', 'Coquitlam');
+insert into branch(location,city) values ('Metrotown', 'Burnaby');
 
 insert into VehicleType (vtname, features, wrate, drate, hrate,wirate,dirate,hirate,krate) values ('Economy', '', 6, 3, 1, 14, 11, 8, 18);
 insert into VehicleType (vtname, features, wrate, drate, hrate,wirate,dirate,hirate,krate) values ('SUV', '', 9, 7, 4, 19, 16, 9, 25);
@@ -113,8 +115,15 @@ insert into Customer (dlicense, name, address, phonenumber) values (1729311, 'Ra
 insert into Customer (dlicense, name, address, phonenumber) values (1123328, 'Raymond Ng', '696 Ontario St.', 5671123);
 insert into Customer (dlicense, name, address, phonenumber) values (0981726, 'Jessica Wong', '66-993 Ontar St.', 5671123);
 
-insert into Vehicle (vlicense, vid, make, model, year, color, odometer, status, vtname, location, city) values (133313333, 1, 'Honda', 'Civic', 2006, 'red', 1080, 'available', 'Economy', 'Blundell and No. 3', 'Richmond');
+insert into Vehicle (vlicense, vid, make, model, year, color, odometer, status, vtname, location, city) values (133313333, 1, 'Honda', 'Civic', 2006, 'red', 1080, 'rented', 'Economy', 'Blundell and No. 3', 'Richmond');
 insert into Vehicle (vlicense, vid, make, model, year, color, odometer, status, vtname, location, city) values (244424444, 2, 'Nissan', 'Murano', 2016, 'red', 80, 'maintenance', 'SUV', 'Waterfront', 'Vancouver');
 insert into Vehicle (vlicense, vid, make, model, year, color, odometer, status, vtname, location, city) values (453423129, 3, 'Nissan', 'Murano', 2013, 'blue', 14400, 'rented', 'SUV', 'Waterfront', 'Vancouver');
 insert into Vehicle (vlicense, vid, make, model, year, color, odometer, status, vtname, location, city) values (888888888, 4, 'Ford', 'Mustang', 2003, 'white', 10012, 'available', 'Mid-size', 'Lougheed', 'Coquitlam');
+insert into Vehicle (vlicense, vid, make, model, year, color, odometer, status, vtname, location, city) values (133313332, 5, 'Honda', 'Civic', 2005, 'red', 0, 'available', 'Economy', 'Blundell and No. 3', 'Richmond');
+insert into Vehicle (vlicense, vid, make, model, year, color, odometer, status, vtname, location, city) values (696969420, 6, 'Tesla', 'Cybertruck', 2019, 'black', 0, 'available', 'Truck', 'Steveston', 'Richmond');
+
+insert into Reservation (vtname, dlicense, fromDate, toDate) values ('SUV', 1729311, TO_DATE('2019/11/29', 'YYYY/MM/DD'), TO_DATE('2019/11/30', 'YYYY/MM/DD'));
+
+insert into Rental (vlicense, dlicense, confNo, fromDate, toDate, cardName, cardNo, expDate) values (133313333, 1729328, null, TO_DATE('2019/11/29', 'YYYY/MM/DD'), TO_DATE('2019/11/30', 'YYYY/MM/DD'), 'Raymond Ng', 123456, TO_DATE('2023/11/19', 'YYYY/MM/DD'));
+
 commit;
