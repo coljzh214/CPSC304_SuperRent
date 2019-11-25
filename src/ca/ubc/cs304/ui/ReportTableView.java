94b217173ab9e97f0b4fa8fe46a9508f1f878fb9
alex587 <alex587@students.cs.ubc.ca>
2019-11-24 20:58:18 -0800

    added ReportTableView

---
+++ src/ca/ubc/cs304/ui/ReportTableView.java
@@ -0,0 +1,76 @@
+package ca.ubc.cs304.ui;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.*;
+import java.util.List;
+import java.util.ArrayList;
+
+import ca.ubc.cs304.model.ReportModel;
+ 
+public class ReportTableView {
+
+    static JScrollPane createTable(List list, String[] columnNames) {
+        Object[][] array = new String[list.size()][];
+        for (int i = 0; i < list.size(); i++) {
+            ArrayList<Object> row = (ArrayList<Object>) list.get(i);
+            array[i] = row.toArray(new String[row.size()]);
+        }
+        JTable table = new JTable(array, columnNames);
+        return new JScrollPane(table);
+    }
+
+    public ReportTableView(ReportModel reportModel) {
+        String[] columnNames = {  "vlicense",  "vid",  "make",  "model",  "year",  "color",  "odometer",
+                 "status",  "vtname",  "location",  "city" };
+        JScrollPane scrollerA = createTable(reportModel.a, columnNames);
+        JScrollPane scrollerB = createTable(reportModel.b, columnNames);
+        JScrollPane scrollerC = createTable(reportModel.c, columnNames);
+        JScrollPane scrollerD = createTable(reportModel.d, columnNames);
+        JLabel tableALabel = new JLabel("IDK");
+        JLabel tableBLabel = new JLabel("IDK");
+        JLabel tableCLabel = new JLabel("IDK");
+        JLabel tableDLabel = new JLabel("IDK");
+
+        JPanel panel = new JPanel();
+        GridBagLayout gb = new GridBagLayout();
+        GridBagConstraints c = new GridBagConstraints();
+        panel.setLayout(gb);
+        // adding the labels
+        c.gridwidth = GridBagConstraints.RELATIVE;
+		gb.setConstraints(tableALabel, c);
+        panel.add(tableALabel);
+        c.gridwidth = GridBagConstraints.REMAINDER;
+		gb.setConstraints(tableBLabel, c);
+        panel.add(tableBLabel);
+        // adding the table
+        c.gridwidth = GridBagConstraints.RELATIVE;
+		gb.setConstraints(scrollerA, c);
+        panel.add(scrollerA);
+        c.gridwidth = GridBagConstraints.REMAINDER;
+		gb.setConstraints(scrollerB, c);
+        panel.add(scrollerB);
+        // adding the labels
+        c.gridwidth = GridBagConstraints.RELATIVE;
+		gb.setConstraints(tableCLabel, c);
+        panel.add(tableCLabel);
+        c.gridwidth = GridBagConstraints.REMAINDER;
+		gb.setConstraints(tableDLabel, c);
+        panel.add(tableDLabel);
+        // adding the table
+        c.gridwidth = GridBagConstraints.RELATIVE;
+		gb.setConstraints(scrollerC, c);
+        panel.add(scrollerC);
+        c.gridwidth = GridBagConstraints.REMAINDER;
+		gb.setConstraints(scrollerD, c);
+        panel.add(scrollerD);
+
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
--- src/ca/ubc/cs304/ui/TableView.java
+++ src/ca/ubc/cs304/ui/TableView.java
@@ -5,43 +5,50 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import java.awt.*;
 
 import ca.ubc.cs304.model.VehicleModel;
-
-public class TableView extends JPanel {
-    // frame
-    JFrame f;
-    // Table
-    JTable j;
-
-    // Constructor
-    TableView(VehicleModel[] vModels) {
-        // Frame initiallization
-        f = new JFrame();
-
-        // Frame Title
-        f.setTitle(vModels.length + " available vehicles");
-
-        // Data to be displayed in the JTable
-        String[][] data = new String[vModels.length][11];
-        for (int i = 0; i < vModels.length; i++) {
-            data[i] = vModels[i].getTuple();
+ 
+public class TableView {
+        // frame 
+        JFrame f; 
+        // Table 
+        JTable j;
+        JPanel p; 
+      
+        // Constructor 
+        TableView(VehicleModel[] vModels) { 
+            // Frame initiallization 
+            f = new JFrame();
+            p = new JPanel();
+            p.setLayout(new BorderLayout());
+            JLabel number = new JLabel("Total number of available vehicles: " + vModels.length);
+      
+            // Frame Title 
+            f.setTitle("JTable Example"); 
+      
+            // Data to be displayed in the JTable 
+            String[][] data = new String[vModels.length][11];
+            for (int i = 0; i < vModels.length; i++) {
+                data[i] = vModels[i].getTuple();
+            }
+      
+            // Column Names 
+            String[] columnNames = {  "vlicense",  "vid",  "make",  "model",  "year",  "color",  "odometer",
+                 "status",  "vtname",  "location",  "city" }; 
+      
+            // Initializing the JTable 
+            j = new JTable(data, columnNames); 
+            j.setBounds(30, 40, 200, 300); 
+      
+            // adding it to JScrollPane 
+            JScrollPane sp = new JScrollPane(j);
+            p.add(number, "North");
+            p.add(sp, "Center");
+            // Frame Size
+            f.getContentPane().add(p); 
+            f.setSize(700, 300); 
+            // Frame Visible = true 
+            f.setVisible(true); 
         }
-
-        // Column Names
-        String[] columnNames = {  "vlicense",  "vid",  "make",  "model",  "year",  "color",  "odometer",
-                "status",  "vtname",  "location",  "city" };
-
-        // Initializing the JTable
-        j = new JTable(data, columnNames);
-        j.setBounds(30, 40, 200, 300);
-
-        // adding it to JScrollPane
-        JScrollPane sp = new JScrollPane(j);
-        f.add(sp);
-        // Frame Size
-        f.setSize(500, 200);
-        // Frame Visible = true
-        f.setVisible(true);
-    }
 } 
